0772105c754666bcd2a5eeeefc1ab0b3e454b14a
Pius Udoma <udomahpius5@gmail.com>
2024-08-22 13:59:06 +0100

    init

--- productforsale/assets/js/timeCount.js
+++ productforsale/assets/js/timeCount.js
@@ -1,21 +1,21 @@
 
 
 
-const interval = () => {
-    setInterval(() => {
-        const getCurrentTime = () => {
-            const timer = document.getElementById('timer')
-            const dT = new Date();
-            const hours = dT.getHours();
-            const minutes = dT.getMinutes();
-            const seconds = dT.getSeconds()
-            timer.innerHTML = `${hours}:${minutes}:${seconds}`
-        }
-        getCurrentTime()
+// const interval = () => {
+//     setInterval(() => {
+//         const getCurrentTime = () => {
+//             const timer = document.getElementById('timer')
+//             const dT = new Date();
+//             const hours = dT.getHours();
+//             const minutes = dT.getMinutes();
+//             const seconds = dT.getSeconds()
+//             timer.innerHTML = `${hours}:${minutes}:${seconds}`
+//         }
+//         getCurrentTime()
         
-    }, 1000);
-}
-interval()
+//     }, 1000);
+// }
+// interval()
 
 
 
