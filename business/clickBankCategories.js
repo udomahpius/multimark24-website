29d2b1008de24484d1fd2b8f326e4752db87bc32
Pius Udoma <udomahpius5@gmail.com>
2024-08-23 23:20:40 +0100

    init

--- business/clickBankCategories.js
+++ business/clickBankCategories.js
@@ -1,5 +1,5 @@
 const ClickBankProduct = {
-  bettingSystems: [
+  businessInvesting: [
     {
       id: 1,
       img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
@@ -24,8 +24,107 @@ const ClickBankProduct = {
       link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
       title: `26 October, 2018 | By Mark Wiens`,
     },
+  ],
+  eBusinessEmarketing: [
     {
-      id: 5,
+      id: 1,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 2,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 3,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 4,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+  ],
+
+  softwareServices: [
+    {
+      id: 1,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 2,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 3,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 4,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+  ],
+  affiliateTools: [
+    {
+      id: 1,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 2,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 3,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 4,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+  ],
+  directOfferTracking: [
+    {
+      id: 1,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 2,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 3,
+      img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
+      link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
+      title: `26 October, 2018 | By Mark Wiens`,
+    },
+    {
+      id: 4,
       img: `<img class="img-fluid" src="./img/latest-blog/l-blog-1.jpg" alt="">`,
       link: `<a href="single-blog.html"><h4>Addiction When Gambling Becomes A Problem</h4></a>`,
       title: `26 October, 2018 | By Mark Wiens`,
@@ -33,14 +132,14 @@ const ClickBankProduct = {
   ],
 };
 
-// ClickBankProduct bettingSystems section
+// ClickBankProduct businessInvesting section 
 
 const ClickBankProductContainer_1 = () => {
   let ClickBankProductHtml_1 = document.querySelector(
     ".html-ClickBankProduct_1"
   );
   let displayClickBankProductItem_1 = "";
-  ClickBankProduct.bettingSystems.map(
+  ClickBankProduct.businessInvesting.map(
     (ClickBankProductItem_1) => {
       const { img, title, link} =
         ClickBankProductItem_1;
@@ -62,4 +161,136 @@ const ClickBankProductContainer_1 = () => {
   );
 };
 ClickBankProductContainer_1();
+
+
+
+// ClickBankProduct eBusinessEmarketing section
+
+const ClickBankProductContainer_2 = () => {
+  let ClickBankProductHtml_2 = document.querySelector(
+    ".html-ClickBankProduct_2"
+  );
+  let displayClickBankProductItem_2 = "";
+  ClickBankProduct.eBusinessEmarketing.map(
+    (ClickBankProductItem_2) => {
+      const { img, title, link} =
+        ClickBankProductItem_2;
+      displayClickBankProductItem_2 += `
+        <div class="col-lg-3 col-md-6">
+						<div class="l_blog_item">
+							<a href="">
+							${img}
+							</a>
+							<p class="date">${title}</p>
+							${link}
+						</div>
+						
+					</div>
+  `;
+      ClickBankProductHtml_2.innerHTML =
+        displayClickBankProductItem_2;
+    }
+  );
+};
+ClickBankProductContainer_2();
+
+
+
+// ClickBankProduct softwareServices section
+
+const ClickBankProductContainer_3 = () => {
+  let ClickBankProductHtml_3 = document.querySelector(
+    ".html-ClickBankProduct_3"
+  );
+  let displayClickBankProductItem_3 = "";
+  ClickBankProduct.softwareServices.map(
+    (ClickBankProductItem_3) => {
+      const { img, title, link} =
+        ClickBankProductItem_3;
+      displayClickBankProductItem_3 += `
+        <div class="col-lg-3 col-md-6">
+						<div class="l_blog_item">
+							<a href="">
+							${img}
+							</a>
+							<p class="date">${title}</p>
+							${link}
+						</div>
+						
+					</div>
+  `;
+      ClickBankProductHtml_3.innerHTML =
+        displayClickBankProductItem_3;
+    }
+  );
+};
+ClickBankProductContainer_3();
+
+
+
+
+// ClickBankProduct affiliateTools section 
+
+const ClickBankProductContainer_4 = () => {
+  let ClickBankProductHtml_4 = document.querySelector(
+    ".html-ClickBankProduct_4"
+  );
+  let displayClickBankProductItem_4 = "";
+  ClickBankProduct.affiliateTools.map(
+    (ClickBankProductItem_4) => {
+      const { img, title, link} =
+        ClickBankProductItem_4;
+      displayClickBankProductItem_4 += `
+        <div class="col-lg-3 col-md-6">
+						<div class="l_blog_item">
+							<a href="">
+							${img}
+							</a>
+							<p class="date">${title}</p>
+							${link}
+						</div>
+						
+					</div>
+  `;
+      ClickBankProductHtml_4.innerHTML =
+        displayClickBankProductItem_4;
+    }
+  );
+};
+ClickBankProductContainer_4();
+
+// ClickBankProduct directOfferTracking section  
+
+const ClickBankProductContainer_5 = () => {
+  let ClickBankProductHtml_5 = document.querySelector(
+    ".html-ClickBankProduct_5"
+  );
+  let displayClickBankProductItem_5 = "";
+  ClickBankProduct.directOfferTracking.map(
+    (ClickBankProductItem_5) => {
+      const { img, title, link} =
+        ClickBankProductItem_5;
+      displayClickBankProductItem_5 += `
+        <div class="col-lg-3 col-md-6">
+						<div class="l_blog_item">
+							<a href="">
+							${img}
+							</a>
+							<p class="date">${title}</p>
+							${link}
+						</div>
+						
+					</div>
+  `;
+      ClickBankProductHtml_5.innerHTML =
+        displayClickBankProductItem_5;
+    }
+  );
+};
+ClickBankProductContainer_5();
+
+
+
+
+
 
