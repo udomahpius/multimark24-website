d5879a788bde474f1b7e0c4403fe08f539557723
Pius Udoma <udomahpius5@gmail.com>
2024-08-27 18:01:41 +0100

    init

--- productforsale/assets/shareAsale-product/lightRoomBestSellers.js
+++ productforsale/assets/shareAsale-product/lightRoomBestSellers.js
@@ -2,154 +2,123 @@
 
 
 const ShareAsaleBestSalesProduct = {
-    lightinthebox_BestSales_1:[
-        {
-            id: 1,
-            img: `.//..//../assets/images/BESTSELLERS/best_1.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m235"><i class="fa fa-search"></i></a>`,
-            mainPrice: ` $52.65`,
-            buyLink: ` <a href="http://shrsl.com/4m235"  class="h6 text-decoration-none ">Women's Mesh Black Dress Floral Embroidered Crew Neck Puff Sleeve </a>`
-
-        },
-        {
-            id: 2,
-            img: `.//..//../assets/images/BESTSELLERS/best_2.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m239"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$24.99 `,
-            buyLink: `<a href="http://shrsl.com/4m239"  class="h6 text-decoration-none "> Blouse Dressy Tops Women's Black White Yellow Solid / Plain Color Sexy Daily Elegant V Neck Regular Fit S</a>`  
-
-
-        },
-        {
-            id: 3,
-            img: `.//..//../assets/images/BESTSELLERS/best_3.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m23c"><i class="fa fa-search"></i></a>`,
-            mainPrice: ` $15.99`,
-            buyLink: `<a href="http://shrsl.com/4m23c"  class="h6 text-decoration-none ">Men's Casual 3D Print Golf Polo Daily Wear Short Sleeve Turndown Polo Shirts</a>`,  
-
-
-        },
-        {
-            id: 4,
-            img: `.//..//../assets/images/BESTSELLERS/best_4.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m23x"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$13.99`,
-            buyLink: `<a href="http://shrsl.com/4m23x"  class="h6 text-decoration-none ">Men's Shorts Summer Shorts Beach Shorts Drawstring Plain Yoga Business Beach 100% Cotton Hawaiian Casual Light Khaki </a>`,  
-
-
-        },
-        {
-            id: 5,
-            img: `.//..//../assets/images/BESTSELLERS/best_5.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m241"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$23.99`,
-            buyLink: `<a href="http://shrsl.com/4m241"  class="h6 text-decoration-none "> Men's Loafers & Slip-Ons Slip-on Sneakers Comfort Shoes Walking Classic Casual Outdoor Daily Canvas </a>`,  
-
-
-        },
-        {
-            id: 6,
-            img: `.//..//../assets/images/BESTSELLERS/best_6.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m243"><i class="fa fa-search"></i></a>`,
-            mainPrice: ` $39.99`,
-            buyLink: `<a href="http://shrsl.com/4m243"  class="h6 text-decoration-none ">Men's Oxfords Embroidery Dress Shoes Plus Size Comfort Shoes Walking Casual Chinoiserie British Daily Party</a>`,  
-
-
-        },
-        {
-            id: 7,
-            img: `.//..//../assets/images/BESTSELLERS/best_7.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m245"><i class="fa fa-search"></i></a>`,
-            mainPrice: ` $17.30`,
-            buyLink: `<a href="http://shrsl.com/4m245"  class="h6 text-decoration-none "> Women's Black Wedge Sandals with Strappy - Fashionable Summer Footwear for Casual and Evening Wear </a>`,  
-
-
-        },
-        {
-            id: 8,
-            img: `.//..//../assets/images/BESTSELLERS/best_8.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4m247"><i class="fa fa-search"></i></a>`,
-            mainPrice: ` $26.90`,
-            buyLink: `<a href="http://shrsl.com/4m247"  class="h6 text-decoration-none "> Women's Swimwear Bikini 2 Piece Swimsuit Backless 2 Piece Push Up Sexy Printing Leaf V Wire Vacation Beach Wear Bathing Suits </a>`,  
-
-
-        },
-    ],
-  lightinthebox_BestSales_2:[
+  lightinthebox_BestSales_1: [
     {
-            id: 1,
-          img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_1.jpg`,
-          iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-            mainPrice: ` $6.99`,
-            buyLink: `<a href="http://shrsl.com/4m24k"  class="h6 text-decoration-none "> Effortless Slicing & Cutting,10 Stainless Steel Wires Multifunctional Slicer for Cheese, Eggs, etc </a>`,  
-
-
+      id: 1,
+      img: `.//..//../assets/images/BESTSELLERS/best_1.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nlx9"><i class="fa fa-search"></i></a>`,
+      mainPrice: ` $16.17`,
+      buyLink: ` <a href="http://shrsl.com/4nlx9"  class="h6 text-decoration-none "> Shoulder Bag Vintage Leather Vertical Hand Business Men's Casual Leather Bag Satchel Bag For Men </a>`,
     },
     {
-            id: 2,
-            img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_2.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$12.99`,
-            buyLink: `<a href="http://shrsl.com/4m24k"  class="h6 text-decoration-none "> Sink Caddy, Sponge Drain Rack With Dishcloth Holder, Stainless Steel Sink Storage  </a>`,  
-
-
+      id: 2,
+      img: `.//..//../assets/images/BESTSELLERS/best_2.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nlxa"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$12.99 `,
+      buyLink: `<a href="http://shrsl.com/4nlxa"  class="h6 text-decoration-none "> Women's Shapewear Casual / Sporty Shorts Scrunch Butt Shorts Anti Chafing Shorts Short Pants</a>`,
     },
     {
-            id: 3,
-            img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_3.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$15.99`,
-            buyLink: `<a href="http://shrsl.com/4m24k"  class="h6 text-decoration-none "> Peeler Stainless Steel Double Layer Multifunction Vegetables and Fruit Grater Home Kitchen Tool</a>`,  
-
-
+      id: 3,
+      img: `.//..//../assets/images/BESTSELLERS/best_3.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nlxe"><i class="fa fa-search"></i></a>`,
+      mainPrice: ` $21.70`,
+      buyLink: `<a href="http://shrsl.com/4nlxe"  class="h6 text-decoration-none ">Women's Sandals Bling Bling Flat Sandals Sparkling Shoes Outdoor Beach Summer Rhinestone Flat Heel </a>`,
     },
     {
-            id: 4,
-            img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_4.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$7.99`,
-            buyLink: `<a href="http://shrsl.com/4m24k"  class="h6 text-decoration-none "> Barbecue Olive Oil Spray Bottle Oil Vinegar Spray Bottle Water Barbecue Grill Sprayer Kitchen Tool</a>`,  
-
-
+      id: 4,
+      img: `.//..//../assets/images/BESTSELLERS/best_4.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nlxh"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$21.70`,
+      buyLink: `<a href="http://shrsl.com/4nlxh"  class="h6 text-decoration-none ">Women's Sandals Plus Size Outdoor Daily Beach Solid Color Summer Zipper High Heel </a>`,
     },
     {
-            id: 5,
-            img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_5.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$16.99`,
-            buyLink: `<a href="http://shrsl.com/4m24k"  class="h6 text-decoration-none "> 4 Tiers Egg Holder for Fridge，Egg Holder for Fridge, etc </a>`,  
-
-
+      id: 5,
+      img: `.//..//../assets/images/BESTSELLERS/best_5.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nlxv"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$20.35`,
+      buyLink: `<a href="http://shrsl.com/4nlxv"  class="h6 text-decoration-none "> Men's Sweatshirt Quarter Zip Sweatshirt Black Dark Gray Half Zip Reindeer Print Christmas Daily Holiday Streetwear </a>`,
     },
     {
-            id: 6,
-            img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_6.jpg`,
-            iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-            mainPrice: `$7.99`,
-            buyLink: `<a href="http://shrsl.com/4m24k"  class="h6 text-decoration-none "> Air Fryer Silicone Bakeware Multi-Functional Barbecue </a>`,  
-
-
-      },
-      {
-        id: 7,
-        img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_7.jpg`,
-        iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-        mainPrice: `$6.99`,
-        buyLink: `<a href="http://shrsl.com/4m24k"  class="h6 text-decoration-none "> 50 Pcs Set Reusable Aluminum Foil Air Fryer Liners Oil-Proof, Water-Proof, etc</a>`,  
-
-
-      },
-      {
-        id: 8,
-        img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_8.jpg`,
-        iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4m24k"><i class="fa fa-search"></i></a>`,
-        mainPrice: `$7.99`,
-        buyLink: `<a href="http://shrsl.com/4m24k"    class="h6 text-decoration-none "> 1pc, Novelty Can Opener Jar Opener Lid Remover Aid Arthritis Weak Hands </a>`,  
-
-
-},
-    ],
-}
+      id: 6,
+      img: `.//..//../assets/images/BESTSELLERS/best_6.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nly3"><i class="fa fa-search"></i></a>`,
+      mainPrice: ` $34.77`,
+      buyLink: `<a href="http://shrsl.com/4nly3"  class="h6 text-decoration-none ">Men's Perforated Leather Slip-On Loafers - Comfortable Casual Driving Shoes</a>`,
+    },
+    {
+      id: 7,
+      img: `.//..//../assets/images/BESTSELLERS/best_7.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nlyd"><i class="fa fa-search"></i></a>`,
+      mainPrice: ` $43.40`,
+      buyLink: `<a href="http://shrsl.com/4nlyd"  class="h6 text-decoration-none "> Men's Casual Faux Leather Slip-On Loafers with Handcrafted Stitching and Velcro Strap </a>`,
+    },
+    {
+      id: 8,
+      img: `.//..//../assets/images/BESTSELLERS/best_8.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" href="http://shrsl.com/4nlyj"><i class="fa fa-search"></i></a>`,
+      mainPrice: ` $11.20`,
+      buyLink: `<a href="http://shrsl.com/4nlyj"  class="h6 text-decoration-none ">Men's Premium Leather Belt with Sleek Stainless Steel Buckle - Perfect for Business and Casual Wear </a>`,
+    },
+  ],
+  lightinthebox_BestSales_2: [
+    {
+      id: 1,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_1.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlyw"><i class="fa fa-search"></i></a>`,
+      mainPrice: ` $5.45`,
+      buyLink: `<a href="http://shrsl.com/4nlyw"  class="h6 text-decoration-none "> 15 Pcs New Style High Quality Makeup Brush Set Rainbow Colors Eyeshadow </a>`,
+    },
+    {
+      id: 2,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_2.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlyx"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$11.69`,
+      buyLink: `<a href="http://shrsl.com/4nlyx"  class="h6 text-decoration-none "> 1PCS Makeup Brush Makeup Tool Cleaning Silicone Folding Bowl </a>`,
+    },
+    {
+      id: 3,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_3.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlyy"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$6.49`,
+      buyLink: `<a href="http://shrsl.com/4nlyy"  class="h6 text-decoration-none "> Makeup Brushes/Bags, 8 Pcs Portable Soft Makeup Brushes Set For Eyeshadow </a>`,
+    },
+    {
+      id: 4,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_4.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlz3"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$5.45`,
+      buyLink: `<a href="http://shrsl.com/4nlz3"  class="h6 text-decoration-none "> 3Pcs Makeup Sponge Beauty Foundation Blending Sponge Makeup Sponge Wedge</a>`,
+    },
+    {
+      id: 5,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_5.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlz7"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$4.39`,
+      buyLink: `<a href="http://shrsl.com/4nlz7"  class="h6 text-decoration-none "> Women's Drop Earrings Fine Jewelry Tassel Fringe Precious Stylish Luxury Earrings  </a>`,
+    },
+    {
+      id: 6,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_6.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlzc"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$4.39`,
+      buyLink: `<a href="http://shrsl.com/4nlzc"  class="h6 text-decoration-none "> Women's Drop Earrings Fine Jewelry Tassel Fringe Precious Cute Stylish Earrings  </a>`,
+    },
+    {
+      id: 7,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_7.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlzh"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$7.69`,
+      buyLink: `<a href="http://shrsl.com/4nlzh"  class="h6 text-decoration-none "> Women's Bracelet Ladies' Classic Stainless Steel Cubic Zircon Inlay Gold </a>`,
+    },
+    {
+      id: 8,
+      img: `.//..//../assets/images/KitchenUtensils&Gadgets/home_8.jpg`,
+      iconLink: `<a class="btn btn-outline-dark btn-square" <a href="http://shrsl.com/4nlzq"><i class="fa fa-search"></i></a>`,
+      mainPrice: `$3.89`,
+      buyLink: `<a href="http://shrsl.com/4nlzq"    class="h6 text-decoration-none "> Women's necklace Chic & Modern Daily Geometry Necklaces / Gold / Silver / Fall / Winter / Spring </a>`,
+    },
+  ],
+};
 
 // ShareAsaleBestSalesProduct lightinthebox_electronics section 
 
