9ad1a786d9391cc2eae810c4c51cf62be88d5678
Pius Udoma <udomahpius5@gmail.com>
2024-08-27 09:53:08 +0100

    init

--- productforsale/collection.js
+++ productforsale/collection.js
@@ -1,121 +1,120 @@
 // ALL JOURNAL PUBLICATIONS ARE MAPPED HERE IN JS FILE AND DISPLAY IN JOURNAL.HTML FILE
  
  const shareAsaleCollect = [
-    {
-        id: 1,
-        img: `<a href="http://shrsl.com/4ljep" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Chinavasion-gadgets.jpg" class="img-fluid alt=""></a>`,
-        title: ` Electronics & Gadgets `,
-        body: ` 2000 Products`,
-        link: `<a href="http://shrsl.com/4ljep" target="_blank" class="flex-link a-style">Shop Now  </a> `
-    },
-    {
-        id: 2,
-        img: `<a href="https://shareasale.com/r.cfm?b=213922&u=3166404&m=25882&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/SleekHair-hair-care.jpg" class="img-fluid"  alt=""></a>`,
-        title: `Hair Care Products  `,
-        body: ` 800 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=213922&u=3166404&m=25882&urllink=&afftrack=" class="flex-link a-style"  target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 3,
-        img: `<a href="https://shareasale.com/r.cfm?b=562574&u=3166404&m=51523&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Daily-Sale.jpg" class="img-fluid alt=""></a>`,
-        title: `Online Deals and Offers `,
-        body: ` 6000 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=562574&u=3166404&m=51523&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 4,
-        img: `<a href="https://shareasale.com/r.cfm?b=794414&u=3166404&m=62397&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/UNGEXPty.webp" class="img-fluid alt=""></a>`,
-        title: `SKIN AND HAIR CARE `,
-        body: ` 900 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=794414&u=3166404&m=62397&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 5,
-        img: `<a href="https://shareasale.com/r.cfm?b=870140&u=3166404&m=65997&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/stylekorean.jpg" class="img-fluid alt=""></a>`,
-        title: `BEAUTY TRENDS `,
-        body: ` 3000 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=870140&u=3166404&m=65997&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 6,
-        img: `<a href="https://shareasale.com/u.cfm?d=452658&m=75076&u=3166404&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Peggybuy.jpg"class="img-fluid alt=""></a>`,
-        title: `diamond paintings `,
-        body: ` 700 Products`,
-        link: `<a href="https://shareasale.com/u.cfm?d=452658&m=75076&u=3166404&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 7,
-        img: `<a href="hhttps://shareasale.com/r.cfm?b=1540810&u=3166404&m=97461&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Kiierr-Internationa.jpg" class="img-fluid alt=""></a>`,
-        title: `Thinning Hair & Hair Loss  `,
-        body: ` 700 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=1540810&u=3166404&m=97461&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 8,
-        img: `<a href="https://shareasale.com/r.cfm?b=2104767&u=3166404&m=128902&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/SKG-Health-Technologies.jpg" class="img-fluid alt=""></a>`,
-        title: `wearable health devices `,
-        body: ` 400 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=2104767&u=3166404&m=128902&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 9,
-        img: `<a href="https://shareasale.com/r.cfm?b=2418913&u=3166404&m=146988&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/MaheTri-LLC.jpg" class="img-fluid alt=""></a>`,
-        title: `Classy Leather Bags `,
-       body: ` 600 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=2418913&u=3166404&m=146988&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now </a> `
-    },
-    {
-        id: 10,
-        img: `<a href="https://shareasale.com/r.cfm?b=22884&u=3166404&m=5846&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Sunshine-Jewelry.png" class="img-fluid alt=""></a>`,
-        title: `Jewelry `,
-        body: ` 2000 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=22884&u=3166404&m=5846&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 11,
-        img: `<a href="http://shrsl.com/4ll2a" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Inlovearts.jpg" class="img-fluid alt=""></a>`,
-        title: `Crafting `,
-        body: ` 500 Products`,
-        link: `<a href="http://shrsl.com/4ll2a" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-    {
-        id: 12,
-        img: `<a href="https://shareasale.com/r.cfm?b=1633582&u=3166404&m=101905&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/FYB-JEWELRY.jpg" class="img-fluid alt=""></a>`,
-        title: `sterling silver jewelry `,
-        body: ` 1000 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=1633582&u=3166404&m=101905&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `
-    },
-    {
-        id: 13,
-        img: `<a href="https://shareasale.com/r.cfm?b=1756312&u=3166404&m=109559&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/popopieshop.jpg" class="img-fluid alt=""></a>`,
-        title: `Kids clothing `,
-        body: ` 700 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=1756312&u=3166404&m=109559&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `
-    },
-    {
-        id: 14,
-        img: `<a href="https://shareasale.com/r.cfm?b=2456376&u=3166404&m=149328&urllink=&afftrack=" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Chengdu-Yuannimeng-Shoes.jpg" class="img-fluid alt=""></a>`,
-        title: `women's shoes `,
-       body: ` 3000 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=2456376&u=3166404&m=149328&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `
-    },
-    {
-        id: 15,
-        img: `<a href=https://shareasale.com/r.cfm?b=570338&u=3166404&m=51900&urllink=www%2Elightinthebox%2Ecom%2Fc%2Fmen%27s%2520selection%5F121932&afftrack=#" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/LightInTheBoxmen.jpg" class="img-fluid alt=""></a>`,
-        title: `men, apparel and other general merchandise `,
-        body: ` 8000 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=570338&u=3166404&m=51900&urllink=www%2Elightinthebox%2Ecom%2Fc%2Fmen%27s%2520selection%5F121932&afftrack=" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `
-    },
-    // combine 15men and 16women
-    {
-        id: 16,
-        img: `<a href="https://shareasale.com/r.cfm?b=1546200&u=3166404&m=51900&urllink=&afftrack=" target="_blank"><img src=".//..//../assets/images/collection-img-bg/LightInTheBoxwomen.jpg" class="img-fluid alt=""></a>`,
-        title: `women, apparel and other general merchandise `,
-        body: ` 9000 Products`,
-        link: `<a href="https://shareasale.com/r.cfm?b=1546200&u=3166404&m=51900&urllink=&afftrack=" class="flex-link a-style"target="_blank">Shop Now  </a> `
-    },
-
-]
+   {
+     id: 1,
+     img: `<a href="http://shrsl.com/4nl3s" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Chinavasion-gadgets.jpg" class="img-fluid alt=""></a>`,
+     title: ` Electronics & Gadgets `,
+     body: ` 2000 Products`,
+     link: `<a href="http://shrsl.com/4nl3s" target="_blank" class="flex-link a-style">Shop Now  </a> `,
+   },
+   {
+     id: 2,
+     img: `<a href="http://shrsl.com/4nl41" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/SleekHair-hair-care.jpg" class="img-fluid"  alt=""></a>`,
+     title: `Hair Care Products  `,
+     body: ` 800 Products`,
+     link: `<a href="http://shrsl.com/4nl41" class="flex-link a-style"  target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 3,
+     img: `<a href="http://shrsl.com/4nl45" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Daily-Sale.jpg" class="img-fluid alt=""></a>`,
+     title: `Online Deals and Offers `,
+     body: ` 6000 Products`,
+     link: `<a href="http://shrsl.com/4nl45" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 4,
+     img: `<a href="http://shrsl.com/4nl4a" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/UNGEXPty.webp" class="img-fluid alt=""></a>`,
+     title: `SKIN AND HAIR CARE `,
+     body: ` 900 Products`,
+     link: `<a href="http://shrsl.com/4nl4a" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 5,
+     img: `<a href="http://shrsl.com/4nl4f" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/stylekorean.jpg" class="img-fluid alt=""></a>`,
+     title: `BEAUTY TRENDS `,
+     body: ` 3000 Products`,
+     link: `<a href="http://shrsl.com/4nl4f" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 6,
+     img: `<a href="http://shrsl.com/4nl4o" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Peggybuy.jpg"class="img-fluid alt=""></a>`,
+     title: `diamond paintings `,
+     body: ` 700 Products`,
+     link: `<a href="http://shrsl.com/4nl4o" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 7,
+     img: `<a href="http://shrsl.com/4nl4q" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Kiierr-Internationa.jpg" class="img-fluid alt=""></a>`,
+     title: `Thinning Hair & Hair Loss  `,
+     body: ` 700 Products`,
+     link: `<a href="http://shrsl.com/4nl4q" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 8,
+     img: `<a href="http://shrsl.com/4nl4u" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/SKG-Health-Technologies.jpg" class="img-fluid alt=""></a>`,
+     title: `wearable health devices `,
+     body: ` 400 Products`,
+     link: `<a href="http://shrsl.com/4nl4u" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 9,
+     img: `<a href="http://shrsl.com/4nl6i" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/MaheTri-LLC.jpg" class="img-fluid alt=""></a>`,
+     title: `Classy Leather Bags `,
+     body: ` 600 Products`,
+     link: `<a href="http://shrsl.com/4nl6i" class="flex-link a-style"target="_blank">Shop Now </a> `,
+   },
+   {
+     id: 10,
+     img: `<a href="http://shrsl.com/4nl6w" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Sunshine-Jewelry.png" class="img-fluid alt=""></a>`,
+     title: `Jewelry `,
+     body: ` 2000 Products`,
+     link: `<a href="http://shrsl.com/4nl6w" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 11,
+     img: `<a href="http://shrsl.com/4nl6y" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Inlovearts.jpg" class="img-fluid alt=""></a>`,
+     title: `Crafting `,
+     body: ` 500 Products`,
+     link: `<a href="http://shrsl.com/4nl6y" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+   {
+     id: 12,
+     img: `<a href="http://shrsl.com/4nl72" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/FYB-JEWELRY.jpg" class="img-fluid alt=""></a>`,
+     title: `sterling silver jewelry `,
+     body: ` 1000 Products`,
+     link: `<a href="http://shrsl.com/4nl72" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `,
+   },
+   {
+     id: 13,
+     img: `<a href="http://shrsl.com/4nl75" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/popopieshop.jpg" class="img-fluid alt=""></a>`,
+     title: `Kids clothing `,
+     body: ` 700 Products`,
+     link: `<a href="http://shrsl.com/4nl75" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `,
+   },
+   {
+     id: 14,
+     img: `<a href="http://shrsl.com/4nl76" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/Chengdu-Yuannimeng-Shoes.jpg" class="img-fluid alt=""></a>`,
+     title: `women's shoes `,
+     body: ` 3000 Products`,
+     link: `<a href="http://shrsl.com/4nl76" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `,
+   },
+   {
+     id: 15,
+     img: `<a href="http://shrsl.com/4nl7a" target="_blank" ><img src=".//..//../assets/images/collection-img-bg/LightInTheBoxmen.jpg" class="img-fluid alt=""></a>`,
+     title: `men, apparel and other general merchandise `,
+     body: ` 8000 Products`,
+     link: `<a href="http://shrsl.com/4nl7a" class="flex-link a-style"target="_blank">Shop Now  <span><ion-icon name="arrow-forward" aria-hidden="true"></ion-icon></span></a> `,
+   },
+   // combine 15men and 16women
+   {
+     id: 16,
+     img: `<a href="http://shrsl.com/4nl7c" target="_blank"><img src=".//..//../assets/images/collection-img-bg/LightInTheBoxwomen.jpg" class="img-fluid alt=""></a>`,
+     title: `women, apparel and other general merchandise `,
+     body: ` 9000 Products`,
+     link: `<a href="http://shrsl.com/4nl7c" class="flex-link a-style"target="_blank">Shop Now  </a> `,
+   },
+ ];
 
 const collectionContainer = () => {
     let collectionHtml = document.querySelector('.html-collection');
